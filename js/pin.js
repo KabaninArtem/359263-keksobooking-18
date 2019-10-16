0ca0e0edb5548f47b93eb0bce6987d07e0630a58
Artem Kabanin <akabanin6@gmail.com>
2019-10-16 06:21:55 +0300

    Add drag implementation

--- js/pin.js
+++ js/pin.js
@@ -8,8 +8,45 @@
   var enableAdForm = window.adForm.enable;
   var PIN_TEMPLATE = window.mock.PIN_TEMPLATE;
 
-  function onPinActivate() {
+  function onPinActivate(evt) {
     activatePage();
+    // dragPin(evt);
+  }
+
+  function dragPin(evt) {
+    evt.preventDefault();
+    console.log('evt - ', evt);
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    }
+
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   }
 
   function createPin(info, template) {
